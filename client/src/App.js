3fef2cb29f7c24156a1f7397f51eba82d78c9440
BreeAnn <breeannbond@gmail.com>
2023-07-24 18:28:49 -0700

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
