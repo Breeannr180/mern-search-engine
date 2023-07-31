ffe6a561950fb845171f8f0b8a52fc475b119a88
BreeAnn <breeannbond@gmail.com>
2023-07-31 13:21:52 -0700

    updated localhost

--- server/models/User.js
+++ server/models/User.js
@@ -35,7 +35,7 @@ const userSchema = new Schema(
 // hash user password
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
+    const saltRounds = 10;// 10 is default
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
 
